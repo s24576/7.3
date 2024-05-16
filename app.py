2e587334d8faa750ce70328e10206f627c8ffb8e
s24576 <91957033+s24576@users.noreply.github.com>
2024-05-16 18:18:23 +0200

    Update app.py

--- app.py
+++ app.py
@@ -11,17 +11,18 @@ class Car(db.Model):
     brand = db.Column(db.String(50), nullable=False)
     model = db.Column(db.String(50), nullable=False)
     year = db.Column(db.Integer, nullable=False)
+    details = db.Column(db.String(50), nullable=False)
 
 @app.route('/cars', methods=['GET'])
 def get_cars():
     year = request.args.get('year')
     if year:
         filtered_cars = Car.query.filter_by(year=year).all()
-        cars = [{"id": car.id, "brand": car.brand, "model": car.model, "year": car.year} for car in filtered_cars]
+        cars = [{"id": car.id, "brand": car.brand, "model": car.model, "year": car.year, "details": car.details} for car in filtered_cars]
         return jsonify(cars)
     else:
         cars = Car.query.all()
-        cars = [{"id": car.id, "brand": car.brand, "model": car.model, "year": car.year} for car in cars]
+        cars = [{"id": car.id, "brand": car.brand, "model": car.model, "year": car.year, "details": car.details} for car in cars]
         return jsonify(cars)
 
 if __name__ == '__main__':
